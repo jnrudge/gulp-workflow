abea736f388a2217bdd64fa4de6aae0c2a693b69
James Rudge <james.rudge@cottages.com>
2017-07-21 16:15:58 +0100

    Updated readme ang gulp file. need to insall a couple of dependencies

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# gulp-workflow
-My workflow for website development using gulp
+# Wordpress Gulp Workflow
+Gulp Workflow for working with Wordpress themes / sites
+Includes:
+- SCSS Compiler
+- Auto prefixer - Work in progress
+- Minifier
+- Source Maps
+- JS Concat / Combine
+- Live browser reload (works with MAMP / WAMP)
+- Linters - Work in progress
+- Project build - Work in progress
+
+## Required File Structure
--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,13 @@
+// Gulp Files for Wordpress / PHP Sites
+// See README.MD for required file structure and comands
+
 'use strict';
+
+// Settings
+var mampUrl = "localhost:8888/jnrudge";
+var siteName = "jnrudge";
+var masterCssFile = "main"; // don't use style.css as this will clash with Wordpress
+var masterJsFile = "functions";
 
 var gulp = require('gulp');
 var browserSync = require ('browser-sync');
@@ -7,38 +16,51 @@ var plugins = require("gulp-load-plugins")({
 	replaceString: /\bgulp[\-.]/
 });
 
-gulp.task('moveSASS', function(){
+// Grab SASS / CSS Files from Bower Components
+gulp.task('vendorSCSS', function(){
 	gulp.src(plugins.mainBowerFiles())
 		.pipe (plugins.filter('.css'))
 		.pipe(gulp.dest('scss/vendor'));
 });
+
+gulp.task('vendorJS', function () {
+	gulp.src(plugins.mainBowerFiles())
+		.pipe(plugins.filter('.js'))
+		.pipe(gulp.dest('js/vendor'));
+});
 	
-gulp.task('compileSASS', function(){
-	return gulp.src('scss/main.scss')
+// Compile SCSS files, master file must be called main.scss - Get's minified in the build process
+gulp.task('compileSCSS', function(){
+	return gulp.src('scss/' + masterCssFile + '.scss')
 		.pipe (plugins.sourcemaps.init())
 		.pipe (plugins.sass())
+		.pipe (plugins.autoprefixer())
 		.pipe (plugins.sourcemaps.write('./'))
 		.pipe (gulp.dest('css'))
-		.pipe(browserSync.reload({
+		.pipe (browserSync.reload({
 			stream: true
 		}));
 });
 
+// Combine JS files, master file will be called functions.js, make sure no files are called functions.js - Gets minified in the build process
 gulp.task('concatJS', function(){
-	return gulp.src(['js/*.js', '!js/functions.js'])
+	return gulp.src(['js/modules/*.js', '!js/' + masterJsFile + '.js'])
 		.pipe (plugins.sourcemaps.init())
-		.pipe (plugins.concat('functions.js'))
+		.pipe (plugins.concat(masterJsFile + '.js'))
 		.pipe (plugins.sourcemaps.write('./'))
-		.pipe (gulp.dest('js'));
+		.pipe (gulp.dest('js'))
+		.pipe (browserSync.reload());
 });
 
+// Minify JS
 gulp.task('minifyJS', function(){
-	return gulp.src('js/functions.js')
+	return gulp.src('js/' + masterJsFile + '.js')
 		.pipe (plugins.uglify())
-		.pipe (plugins.rename('functions.min.js'))
+		.pipe (plugins.rename(masterJsFile + '.min.js'))
 		.pipe (gulp.dest('js'));
 });
 
+// Minify CSS
 gulp.task('minifyCSS', function(){
 	return gulp.src('css/*.css')
 		.pipe (plugins.sourcemaps.init())
@@ -50,10 +72,12 @@ gulp.task('minifyCSS', function(){
 		.pipe (gulp.dest('css'));
 });
 
+// Clean up Gulp created files - work in progress
 gulp.task('clean', function(){
 	plugins.del(['dist', 'css/mainnpm.css*', 'js/app*.js*']);
 });
 
+// Linters - Work in Progess
 gulp.task('lintSASS', function(){
 	return gulp.src('scss/*.scss')
 		.pipe (plugins.sassLint())
@@ -72,19 +96,28 @@ gulp.task('lintJS',  function(){
 		.pipe (plugins.jshint.reporter('default'));
 });
 
-gulp.task('build', function(){
-	gulp.src('**/*.php')
-		.pipe (gulp.dest('../jnrudge-dist'));
+// Build task - Work in progess
+gulp.task('build', ['minifyCSS', 'minifyJS'],function(){
+	gulp.src(['**/*.php', 'style.css'])
+		.pipe (gulp.dest('../' + siteName));
+	gulp.src('css/' + masterCssFile + '.min.css')
+		.pipe(plugins.cssnano())
+		.pipe(gulp.dest('../' + siteName + '/css'));
+	gulp.src('js/' + masterJsFile + '.min.js')
+		.pipe(plugins.uglify())
+		.pipe(gulp.dest('../ ' + siteName + '/js'));
 });
 
+// Browser sync init
 gulp.task('browserSync', function(){
 	browserSync.init({
-		proxy: 'localhost:8888/jnrudge'
+		proxy: mampUrl
 	})
 });
 
+// Defaut 'main' task
 gulp.task('default', ['browserSync'], function(){
 	gulp.watch('scss/**/*.scss', ['compileSASS']);
+	gulp.watch('js/**/*.js', ['concatJS']);
 	gulp.watch('*.php').on('change', browserSync.reload);
-	gulp.watch('js/**/*.js').on('change', browserSync.reload);
 });
